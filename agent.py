050b4fd3ba23b68f91b41ebe062a98034c4b4521
Kamsi Kodi <kamsikodi@gmail.com>
2025-02-02 07:33:01 +0100

    Code refactored

--- agent.py
+++ agent.py
@@ -23,8 +23,8 @@ tavil = TavilySearchResults(
     max_results=5,
     search_depth="advanced",
     include_answer=True,
-    include_raw_content=True,
-    include_images=True,
+    include_raw_content=False,
+    include_images=False,
 )
 
 # ------------------------------------------------------------------------------
